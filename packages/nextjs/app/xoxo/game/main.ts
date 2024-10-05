56eb9bb151653d6bb609cc21e1e0436d88402f71
Paul Gadi <paulgadi@gmail.com>
2024-10-05 17:21:29 -0400

    added example phaser game

---
+++ packages/nextjs/app/xoxo/game/main.ts
@@ -0,0 +1,22 @@
+import InGame from "./scenes/InGame";
+import MainMenu from "./scenes/MainMenu";
+import Matchmaking from "./scenes/Matchmaking";
+import { AUTO, Game } from "phaser";
+
+//  Find out more information about the Game Config at:
+//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+const config: Phaser.Types.Core.GameConfig = {
+  type: AUTO,
+  width: 414, // 1024,
+  height: 736, // 768,
+  mode: Phaser.Scale.RESIZE,
+  parent: "game-container",
+  backgroundColor: "#028af8",
+  scene: [MainMenu, InGame, Matchmaking],
+};
+
+const StartGame = (parent: string) => {
+  return new Game({ ...config, parent });
+};
+
+export default StartGame;
--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,7 +4,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -20,10 +20,16 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/",
   },
   {
+    label: "Tic-Tac-Toe",
+    href: "/xoxo",
+  },
+  /*
+  {
     label: "Debug Contracts",
     href: "/debug",
     icon: <BugAntIcon className="h-4 w-4" />,
   },
+  */
 ];
 
 export const HeaderMenuLinks = () => {
